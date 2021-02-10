9c603cfe4afd689a7be61290ba0a7e4fe5c200ac
FredericBentz <frederic.bentz@gmail.com>
2021-02-10 13:32:29 +0100

    update

---
+++ HandballScraper/connect.exemple.py
@@ -0,0 +1,9 @@
+
+# rename this file "connect"
+
+def connect_info(self):
+    host = 'host',
+    user = 'user',
+    passwd = 'password',
+    database = 'db'
+    return host, user, passwd, database
--- HandballScraper/connect.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector
-
-
-def connect_info(self):
-    host = 'localhost',
-    user = 'root',
-    passwd = '',
-    database = 'handball'
-    return host, user, passwd, database
